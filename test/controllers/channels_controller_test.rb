64c57f07a02d4e8d2138ac23c1902142a89d88ff
iroha168G <iroha.net168@gmail.com>
2026-02-28 19:55:01 +0900

    修正

--- test/controllers/channels_controller_test.rb
+++ test/controllers/channels_controller_test.rb
@@ -3,18 +3,11 @@ require "test_helper"
 class ChannelsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
-    log_in_as(@user)
+    log_in_as(@user)   # ← これが必要
   end
 
   test "should get index" do
     get channels_url
     assert_response :success
-  end
-
-  test "should create channel" do
-    post channels_url, params: {
-      channel: { channel_id: "abc123" }
-    }
-    assert_response :redirect
   end
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -3,13 +3,11 @@ require "test_helper"
 class UserMailerTest < ActionMailer::TestCase
   test "password_reset" do
     user = users(:one)
-    user.create_reset_digest   # ← これが必須
-
     mail = UserMailer.password_reset(user)
 
     assert_equal "Password reset", mail.subject
     assert_equal [ user.email ], mail.to
     assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match "パスワード", mail.body.encoded
   end
 end
