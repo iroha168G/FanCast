ad59b9982dcba3900beecb3865ec3ea47122a01d
iroha168G <iroha.net168@gmail.com>
2026-02-28 20:05:18 +0900

    修正

--- test/controllers/channels_controller_test.rb
+++ test/controllers/channels_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class ChannelsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
-    log_in_as(@user)   # ← これが必要
+    log_in_as(@user)
   end
 
   test "should get index" do
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -3,11 +3,17 @@ require "test_helper"
 class UserMailerTest < ActionMailer::TestCase
   test "password_reset" do
     user = users(:one)
+
+    user.create_reset_digest
+
     mail = UserMailer.password_reset(user)
 
     assert_equal "Password reset", mail.subject
-    assert_equal [ user.email ], mail.to
-    assert_equal [ "from@example.com" ], mail.from
-    assert_match "パスワード", mail.body.encoded
+    assert_equal [user.email], mail.to
+    assert_equal ["from@example.com"], mail.from
+
+    body = mail.text_part.body.decoded
+    assert_match "パスワード", body
+    assert_match user.reset_token, body
   end
 end
