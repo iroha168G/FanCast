53b5f66dc4e81bfa07a6e2ae4b368b98fefa14f9
iroha168G <iroha.net168@gmail.com>
2026-03-09 08:37:17 +0900

    設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,9 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # 独自ドメイン
+  config.hosts << "fancast.jp"
+  # サブドメイン
+  config.hosts << "www.fancast.jp"
 end
