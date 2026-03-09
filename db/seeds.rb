288834ea2d0799d44217437cb24616cc23902566
iroha168G <iroha.net168@gmail.com>
2026-03-09 22:32:57 +0900

    seedの設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+[
+  { email: "sample1@fancast.jp", name: "sample1", mock_user: false },
+  { email: "sample2@fancast.jp", name: "sample2", mock_user: true }
+].each do |user_data|
+  User.find_or_create_by!(email: user_data[:email]) do |u|
+    u.name = user_data[:name]
+    u.password = "password123"
+    u.password_confirmation = "password123"
+    u.activated = true
+    u.mock_user = user_data[:mock_user]
+  end
+end
